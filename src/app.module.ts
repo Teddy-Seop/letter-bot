d425c87f6264e381e0d90104628358d0b35764b5
Teddy-Seop <teddy.kim@onda.me>
2022-02-20 15:29:43 +0900

    Add absolute path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CrawlerModule } from './module/crawler';
+import { SchedulerModule } from './module/scheduler';
 
 @Module({
-  imports: [CrawlerModule],
+  imports: [CrawlerModule, SchedulerModule],
   controllers: [AppController],
   providers: [AppService],
 })
